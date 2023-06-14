9420e2fe04dbfff5719941c2890134bf9ea95639
kirillmorgunov <kirillmorgunov@mail.ru>
2023-06-14 21:24:22 +0400

    add svg for buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,15 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h3">Текст</Htag>
-      <Button appearance="primary">Кнопка</Button>
-      <Button appearance="ghost">Кнопка</Button>
+      <Button appearance="primary" arrow="down">
+        Кнопка
+      </Button>
+      <Button appearance="primary" arrow="down">
+        Кнопка
+      </Button>
+      <Button appearance="ghost" arrow="right">
+        Кнопка
+      </Button>
     </>
   );
 }
