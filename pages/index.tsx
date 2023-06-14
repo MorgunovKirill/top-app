c2da204bd5e5fb0800083b003bf43c6cc06d81a0
kirillmorgunov <kirillmorgunov@mail.ru>
2023-06-14 20:52:25 +0400

    add htag and button components, styles

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button, Htag } from "../components";
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Htag tag="h3">Текст</Htag>
+      <Button appearance="primary">Кнопка</Button>
+      <Button appearance="ghost">Кнопка</Button>
+    </>
+  );
+}
